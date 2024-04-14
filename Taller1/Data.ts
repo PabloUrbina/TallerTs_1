98dea0d22678551ecdcda0f836fbbae75a5e998d
PabloUrbina <123499194+PabloUrbina@users.noreply.github.com>
2024-04-14 18:55:11 -0500

    Add files via upload

---
+++ Taller1/Data.ts
@@ -0,0 +1,13 @@
+// File: data.ts
+import Serie from './Serie';
+
+const seriesData: Serie[] = [
+    new Serie(1, 'Breaking Bad', 'AMC', 5),
+    new Serie(2, 'Game of Thrones', 'HBO', 8),
+    new Serie(3, 'Stranger Things', 'Netflix', 3),
+    new Serie(4, 'The Crown', 'Netflix', 4),
+    new Serie(5, 'The Mandalorian', 'Disney+', 2),
+    new Serie(6, 'The Witcher', 'Netflix', 2),
+];
+
+export default seriesData;
